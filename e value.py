801a7b26e0fe81e9d842251b2ac18732652b034f
SR-CL-07 <dummydum460369@gmail.com>
2019-11-08 11:22:53 +0530

    Edit in e-value(powers of e)

--- e value.py
+++ e value.py
@@ -1,6 +1,8 @@
 from math import *
+
 n = int(input('Enter accuracy:'))
+x = float(input('Enter power of e'))
 e = 0
-for i in range(n+1):
-    e += 1/(factorial(i))
+for i in range(n + 1):
+    e += (x ** i) / (factorial(i))
 print(e)
